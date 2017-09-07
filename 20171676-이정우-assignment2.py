ffe23cce2db40d9cdfa0799dffd5bc88cd142e36
LeeJungWoo <lmwljw98@naver.com>
2017-09-07 09:21:03 +0900

    LeeJungWoo First Code Added

---
+++ 20171676-이정우-assignment2.py
@@ -0,0 +1,16 @@
+usr_input = int(input("Input Number : "))
+
+while usr_input != -1:
+
+    answer = 1
+
+    for i in range(usr_input, 0, -1):
+        answer *= i
+
+    if usr_input != 0 and not usr_input < -1:
+        print(str(usr_input) + "! = " + str(answer))
+
+    else:
+        print("Wrong input")
+
+    usr_input = int(input("Input Number : "))
