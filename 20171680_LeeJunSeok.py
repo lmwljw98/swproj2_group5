36ee3ddcad6f8f74e1b861b6cf33e70e55881dbc
JunSeok Lee <lgh009833@kookmin.ac.kr>
2017-09-07 09:49:26 +0900

    Factorial v2

--- 20171680_LeeJunSeok.py
+++ 20171680_LeeJunSeok.py
@@ -1,12 +1,12 @@
-while 1 :
-    number = int(input("Enter a number:"))
+number = int(input("Enter a number:"))
+
+while number != -1:
     factorial = 1
 
-    if number >= 0 :
-        for i in range(1, number+1) :
-            factorial = i * factorial
-        print(number,"!:",factorial)
-    elif number == -1 :
-        break
-    else :
-        print("정수를 입력해주세요.")
+    if number >= 0:
+        for i in range(1, number+1):
+            factorial *= i
+        print(number, "!:", factorial)
+    else:
+        print("양수를 입력해주세요.")
+    number = int(input("Enter a number:"))
