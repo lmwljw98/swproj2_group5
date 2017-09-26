206ee1e66c8cab5cc27b0d5cd1f5c5a56a5a6a56
yellowoov <yellowoov@kookmin.ac.kr>
2017-09-26 09:18:27 +0900

    Update 20171678이정훈-assignment4.py

--- assignment4/20171678이정훈-assignment4.py
+++ assignment4/20171678이정훈-assignment4.py
@@ -1,4 +1,4 @@
-def combination(n, m):
+def combination_rec(n, m):
 
     if m == 0:
         return 1
@@ -10,13 +10,26 @@ def combination(n, m):
         return 0
 
     else:
-        return combination(n-1, m-1) + combination(n-1, m)
+        return combination_rec(n-1, m-1) + combination_rec(n-1, m)
 
-    print("%dC%d = %d" %(n, m, combination(n, m)))
+    print("%dC%d = %d" %(n, m, combination_rec(n, m)))
+
+
+def factorial(n):
+    res = 1
+    for i in range(1, n+1):
+        res *= i
+
+    return res
+
+def combination(n, m):
+   print("%dC%d = %d" %(n, m, factorial(n) // factorial(m) // factorial(n-m)))
 
 n = int(input("Enter n: "))
 m = int(input("Enter m: "))
 while n != -1:
-    combination(n, m)
+    combination_rec(n, m)
     n = int(input("Enter n: "))
     m = int(input("Enter m: "))
+
+    
