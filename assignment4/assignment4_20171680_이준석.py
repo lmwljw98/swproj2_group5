a8ca6120eb0e5c1fa3abb0cf583566698c9a35fb
asegh12 <lgh009833@kookmin.ac.kr>
2017-09-26 09:09:10 +0900

    assignment4 commit

---
+++ assignment4/assignment4_20171680_이준석.py
@@ -0,0 +1,22 @@
+def Combination (n, m):
+    if n == 1 or m == n or m == 0:
+        return 1
+    elif n <= -1:
+        return -1
+    else:
+        return Combination(n-1, m) + Combination(n-1, m-1)
+
+n = int(input("Enter n:"))
+m = int(input("Enter m:"))
+
+if n >= m or (n and m) >= 0:
+    while n < m or (n and m) < 0:
+        n = int(input("Enter n:"))
+        m = int(input("Enter m:"))
+
+while n != -1:
+    C = Combination(n, m)
+    print("C(%d, %d) = %d" %(n, m, C))
+    n = int(input("Enter n:"))
+    if n != -1:
+        m = int(input("Enter m:"))
